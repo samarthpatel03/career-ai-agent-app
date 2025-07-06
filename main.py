b1914903be6c0ea8b318e4ae3918026fa93511e1
Samarth Patel <samarthpatel6174@gmail.com>
2025-07-07 00:00:51 +0530

    Updated with new features / bug fixes

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.agent.career_agent import run_career_agent
+from app.schemas.request import CareerQuery
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def root():
+    return {"message": "Agent is running"}
+
+@app.post("/suggest")  # ✅ POST instead of GET
+def suggest_career(query: CareerQuery):
+    result = run_career_agent(query.interests)
+    return result
+
+
+
 # from fastapi import FastAPI
 # from fastapi.middleware.cors import CORSMiddleware
 # from app.agent.career_agent import run_career_agent
@@ -31,26 +57,3 @@
 #     return result
 
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.agent.career_agent import run_career_agent
-from app.schemas.request import CareerQuery
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
-@app.get("/")
-def root():
-    return {"message": "Agent is running"}
-
-@app.post("/suggest")  # ✅ POST instead of GET
-def suggest_career(query: CareerQuery):
-    result = run_career_agent(query.interests)
-    return result
